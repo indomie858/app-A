d510d9f423bf82963442961aa4c3df68ca0120e4
Gaven Grantz <55901980+indomie858@users.noreply.github.com>
2020-09-28 00:01:14 -0700

    Merge Issue17 branch into dev (#25)
    * Added dependency for Android material components. Cleaned up MainActivity for main screen
    
    * Implement material components theme
    
    * Created bottom nav bar
    
    * Fixed labels on bottom nav menu
    
    * Added icons
    
    * Added buttons to home screen
    
    * Fixed bottom nav bar height
    
    * Updated button font size
    
    * Removed save location button
    
    * Added top app bar to UI
    
    * Modified theme and colors on home page
    
    * Resized buttons and changed colors
    
    Also locked activity to portrait
    
    * Fixed constraint
    
    * Fixed constraint
    
    * Fixed constraint
    
    * Created MapActivity
    
    Basic map implementation from mapbox sdk
    Fixed manifest

---
+++ app/src/main/java/com/example/appa/MapActivity.java
@@ -0,0 +1,92 @@
+package com.example.appa;
+
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.mapbox.mapboxsdk.Mapbox;
+import com.mapbox.mapboxsdk.maps.MapView;
+import com.mapbox.mapboxsdk.maps.MapboxMap;
+import com.mapbox.mapboxsdk.maps.OnMapReadyCallback;
+import com.mapbox.mapboxsdk.maps.Style;
+
+
+/**
+ * The most basic example of adding a map to an activity.
+ */
+public class MapActivity extends AppCompatActivity {
+
+    private MapView mapView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+// Mapbox access token is configured here. This needs to be called either in your application
+// object or in the same activity which contains the mapview.
+        Mapbox.getInstance(this, getString(R.string.mapbox_access_token));
+
+// This contains the MapView in XML and needs to be called after the access token is configured.
+        setContentView(R.layout.activity_map);
+
+        mapView = findViewById(R.id.mapView);
+        mapView.onCreate(savedInstanceState);
+        mapView.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(@NonNull MapboxMap mapboxMap) {
+                mapboxMap.setStyle(Style.MAPBOX_STREETS, new Style.OnStyleLoaded() {
+                    @Override
+                    public void onStyleLoaded(@NonNull Style style) {
+
+// Map is set up and the style has loaded. Now you can add data or make other map adjustments.
+
+                    }
+                });
+            }
+        });
+    }
+
+    // Add the mapView lifecycle to the activity's lifecycle methods
+    @Override
+    public void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        mapView.onStart();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mapView.onDestroy();
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mapView.onSaveInstanceState(outState);
+    }
+}
