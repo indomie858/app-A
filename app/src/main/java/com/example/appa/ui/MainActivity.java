7f48211446c96d087fd3c0c35a1012ff0ce69614
Ryunosuke <ryunosuke.mori.294>
2020-09-29 23:46:04 +0900

    Add Setting layout, connect to bottom navigation

--- app/src/main/java/com/example/appa/ui/MainActivity.java
+++ app/src/main/java/com/example/appa/ui/MainActivity.java
@@ -7,7 +7,8 @@ import android.os.Bundle;
 import android.view.View;
 
 import com.example.appa.R;
-
+import com.google.android.material.bottomnavigation.BottomNavigationItemView;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +20,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnNavigationItemSelectedListener((item) ->{
+            switch (item.getItemId()){
+                case R.id.settings_button:
+                    Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+                    startActivity(intent);
+                    break;
+            }
+            return false;
+        });
     }
 
     public void openMapActivity(View view) {
