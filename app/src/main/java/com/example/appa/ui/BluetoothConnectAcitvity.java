cede0311312636c57d1f99054219a5e1940fad8d
Allan <allanhuidobro@gmail.com>
2020-10-01 18:26:11 -0700

    Enable/Disable BT on mobile device

--- app/src/main/java/com/example/appa/ui/BluetoothConnectAcitvity.java
+++ app/src/main/java/com/example/appa/ui/BluetoothConnectAcitvity.java
@@ -2,15 +2,80 @@ package com.example.appa.ui;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Intent;
+import android.content.IntentFilter;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import com.example.appa.R;
 
 public class BluetoothConnectAcitvity extends AppCompatActivity {
 
+    private static final int REQUEST_ENABLE_BT = 0;
+
+    TextView mStatusBT, mPairedBT;
+    Button mEnabledBT;
+
+    BluetoothAdapter btAdapter;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bluetooth_connect_acitvity);
+
+        mStatusBT =  findViewById(R.id.statusBT);
+        mPairedBT = findViewById(R.id.pairedBT);
+        mEnabledBT = findViewById(R.id.enableBT);
+
+        //adapter
+        btAdapter = BluetoothAdapter.getDefaultAdapter();
+
+        //check if bt is available
+        if(btAdapter == null)
+        {
+            mStatusBT.setText("Bluetooth is not available on this device");
+        }
+        else
+        {
+            mStatusBT.setText("Bluetooth is available on this device");
+        }
+
+        //prompt user to enable bluetooth
+        if (!btAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        if(!btAdapter.isEnabled())
+        {
+            mStatusBT.setText("Please enable Bluetooth");
+        }
+        else
+        {
+            mStatusBT.setText("Bluetooth is enabled");
+        }
+        //offer opportunity to enable bt through button
+        mEnabledBT.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (!btAdapter.isEnabled()) {
+                    Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+                    startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+                }
+                if(!btAdapter.isEnabled())
+                {
+                    mStatusBT.setText("Please enable Bluetooth");
+                }
+                else
+                {
+                    mStatusBT.setText("Bluetooth is enabled");
+                }
+            }
+        });
+
     }
 }
