5d379280b9ee8bb18cdd4af047d33dc42939dbc5
Gaven <gavengrantz@gmail.com>
2020-10-03 11:31:51 -0700

    Created functions for forward and reverse geocoding

--- app/src/main/java/com/example/appa/ui/DirectionsActivity.java
+++ app/src/main/java/com/example/appa/ui/DirectionsActivity.java
@@ -6,11 +6,18 @@ import android.util.Log;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.appa.R;
+import com.mapbox.api.directions.v5.DirectionsCriteria;
 import com.mapbox.api.directions.v5.models.DirectionsResponse;
 import com.mapbox.api.directions.v5.models.DirectionsRoute;
+import com.mapbox.api.geocoding.v5.GeocodingCriteria;
+import com.mapbox.api.geocoding.v5.MapboxGeocoding;
+import com.mapbox.api.geocoding.v5.models.CarmenFeature;
+import com.mapbox.api.geocoding.v5.models.GeocodingResponse;
 import com.mapbox.geojson.Point;
 import com.mapbox.mapboxsdk.Mapbox;
 import com.mapbox.services.android.navigation.v5.navigation.NavigationRoute;
+
+import java.util.List;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -35,6 +42,7 @@ public class DirectionsActivity extends AppCompatActivity {
                 .accessToken(Mapbox.getAccessToken())
                 .origin(origin)
                 .destination(destination)
+                .profile(DirectionsCriteria.PROFILE_WALKING)    //selects walking navigation profiles
                 .build()
                 .getRoute(new Callback<DirectionsResponse>() {
                     @Override
@@ -48,7 +56,7 @@ public class DirectionsActivity extends AppCompatActivity {
                             Log.e(TAG, "No routes found");
                             return;
                         }
-
+                        //currentRoute contains JSON response with route details
                         currentRoute = response.body().routes().get(0);
                     }
 
@@ -57,5 +65,69 @@ public class DirectionsActivity extends AppCompatActivity {
                         Log.e(TAG, "Error: " + throwable.getMessage());
                     }
                 });
+    }
+
+    //Converts location address string into geographic coordinates
+    private void getGeocodeForward(String address) {
+        MapboxGeocoding mapboxGeocoding = MapboxGeocoding.builder()
+                .accessToken(String.valueOf(R.string.mapbox_access_token))
+                .query(address)
+                .build();
+
+        mapboxGeocoding.enqueueCall(new Callback<GeocodingResponse>() {
+            @Override
+            public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+                List<CarmenFeature> results = response.body().features();
+                if (results.size() > 0) {
+                    // Log the first results Point.
+                    Point firstResultPoint = results.get(0).center();
+                    Log.d(TAG, "onResponse: " + firstResultPoint.toString());
+
+                } else {
+                    // No result for your request were found.
+                    Log.d(TAG, "onResponse: No result found");
+                }
+            }
+
+            //add code to do something with geocode here
+
+            @Override
+            public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
+    }
+
+    //Converts geographic coordinates into location address string
+    private void getGeocodeReverse(double longitude, double latitude) {
+        MapboxGeocoding reverseGeocode = MapboxGeocoding.builder()
+                .accessToken(String.valueOf(R.string.mapbox_access_token))
+                .query(Point.fromLngLat(longitude, latitude))
+                .geocodingTypes(GeocodingCriteria.TYPE_ADDRESS)
+                .build();
+
+        reverseGeocode.enqueueCall(new Callback<GeocodingResponse>() {
+            @Override
+            public void onResponse(Call<GeocodingResponse> call, Response<GeocodingResponse> response) {
+
+                List<CarmenFeature> results = response.body().features();
+
+                if (results.size() > 0) {
+                    // Log the first results Point.
+                    Point firstResultPoint = results.get(0).center();
+                    Log.d(TAG, "onResponse: " + firstResultPoint.toString());
+                } else {
+                    // No result for your request were found.
+                    Log.d(TAG, "onResponse: No result found");
+                }
+            }
+
+            //add code to do something with address here.
+
+            @Override
+            public void onFailure(Call<GeocodingResponse> call, Throwable throwable) {
+                throwable.printStackTrace();
+            }
+        });
     }
 }
