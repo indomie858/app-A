0fd3dbcb07aaaaa7b877145ecb7fb8f74219b677
Gaven <gavengrantz@gmail.com>
2020-10-18 11:41:53 -0700

    Created layout for TutorialFragment

--- app/src/main/java/com/example/appa/ui/tutorial/TutorialFragment.kt
+++ app/src/main/java/com/example/appa/ui/tutorial/TutorialFragment.kt
@@ -14,7 +14,11 @@ class TutorialFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_tutorial, container, false)
+        val view: View = inflater.inflate(R.layout.fragment_tutorial, container, false)
+
+
+
+        return view
     }
 
 
