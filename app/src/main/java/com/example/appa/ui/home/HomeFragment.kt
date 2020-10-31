e994e14dde7918791fca83d4c8c974a89bcac865
Anne Xaymountry <anne.xaymountry.438@my.csun.edu>
2020-10-30 19:40:03 -0700

    Added GridView

--- app/src/main/java/com/example/appa/ui/home/HomeFragment.kt
+++ app/src/main/java/com/example/appa/ui/home/HomeFragment.kt
@@ -1,28 +1,28 @@
 package com.example.appa.ui.home
 
-import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Button
+import android.widget.GridView
 import androidx.fragment.app.Fragment
 import com.example.appa.R
-import com.example.appa.ui.BluetoothDialog
 import com.example.appa.ui.navigationlist.NavigationListActivity
 
 
 class HomeFragment : Fragment(), View.OnClickListener {
+    var gridView: GridView? = null
+
+    var numWord = arrayOf("Classroom", "Study", "Services", "Food&Drinks", "Sports&Fitness", "Shopping")
+
+
+
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                               savedInstanceState: Bundle?): View? {
 
         val view: View = inflater.inflate(R.layout.fragment_home, container, false)
-        val connectButton: Button = view.findViewById(R.id.connect_button)
-        val directoryButton: Button = view.findViewById(R.id.directory_button)
 
-        connectButton.setOnClickListener(this)
-        directoryButton.setOnClickListener(this)
 
         return view
     }
@@ -33,21 +33,7 @@ class HomeFragment : Fragment(), View.OnClickListener {
     }
 
     override fun onClick(v: View?) {
-        when (v?.id) {
 
-            R.id.directory_button -> {
-                val intent = Intent (getActivity(), NavigationListActivity::class.java)
-                getActivity()?.startActivity(intent)
-            }
-
-            R.id.connect_button -> {
-                val intent = Intent (getActivity(), BluetoothDialog::class.java)
-                getActivity()?.startActivity(intent)
-            }
-
-            else -> {
-            }
-        }
     }
 
 
