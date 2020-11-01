f51ff5d078a05013f8d8656845e19860bbdc219d
Daniel <dpalencia2112@gmail.com>
2020-11-01 12:28:33 -0800

    Implementing Location Distances

--- app/src/main/java/com/example/appa/ui/navigationlist/PlaceAdapter.java
+++ app/src/main/java/com/example/appa/ui/navigationlist/PlaceAdapter.java
@@ -1,11 +1,14 @@
-package com.example.appa.ui;
+package com.example.appa.ui.navigationlist;
 
 import android.content.Context;
 import android.content.Intent;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
@@ -14,21 +17,33 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.example.appa.R;
 import com.example.appa.databinding.PlaceTileBinding;
 import com.example.appa.db.PlaceEntity;
-import com.example.appa.ui.navigation.CustomDirectionsActivity;
+import com.example.appa.model.Place;
 import com.example.appa.ui.navigation.InstructionViewActivity;
+import com.example.appa.viewmodel.PlaceViewModel;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import static android.content.Context.LOCATION_SERVICE;
+import static androidx.core.content.ContextCompat.getSystemService;
+
 public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHolder>  {
     // For debugging
     public static final String TAG = "RecyclerviewAdapter";
-
     // Our list of places
-    private List<PlaceEntity> mPlaces = new ArrayList<>();
+    private List<PlaceEntity> mPlaceEntities = new ArrayList<>();
+    private List<PlaceViewModel> mPlaceViewModels = new ArrayList<>();
+
+
 
     public void setPlaces(List<PlaceEntity> places) {
-        this.mPlaces = places;
+        this.mPlaceEntities = places;
+
+        for (PlaceEntity placeEntity : mPlaceEntities) {
+            PlaceViewModel placeViewModel = new PlaceViewModel(placeEntity);
+            mPlaceViewModels.add(placeViewModel);
+        }
+        // Recyclerview thing to let it know our data has changed
         notifyDataSetChanged();
     }
 
@@ -38,9 +53,11 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHol
     public static class PlaceViewHolder extends RecyclerView.ViewHolder {
         // each data item is just a string in this case
         final PlaceTileBinding binding;
-        public PlaceViewHolder(PlaceTileBinding binding) {
-                super(binding.getRoot());
-                this.binding = binding;
+        final LocationManager manager;
+        public PlaceViewHolder(PlaceTileBinding binding, LocationManager manager) {
+            super(binding.getRoot());
+            this.binding = binding;
+            this.manager = manager;
         }
     }
 
@@ -55,7 +72,8 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHol
     @NonNull
     public PlaceAdapter.PlaceViewHolder onCreateViewHolder(ViewGroup parent,  int viewType) {
         PlaceTileBinding binding = DataBindingUtil.inflate(LayoutInflater.from(parent.getContext()), R.layout.place_tile, parent, false);
-        return new PlaceViewHolder(binding);
+        LocationManager mLocationManager = (LocationManager) parent.getContext().getSystemService(LOCATION_SERVICE);
+        return new PlaceViewHolder(binding, mLocationManager);
     }
 
 
@@ -63,8 +81,11 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHol
     public void onBindViewHolder(@NonNull PlaceViewHolder holder, int position) {
         // This method is called any time
         // an item is added to the list.
-        PlaceEntity currentPlace = mPlaces.get(position);
-        holder.binding.setPlace(currentPlace);
+        PlaceViewModel currentPlaceViewModel = mPlaceViewModels.get(position);
+        currentPlaceViewModel.setLocationManager(holder.manager);
+        holder.binding.setPlace(currentPlaceViewModel);
+
+        LocationManager manager = holder.manager;
 
         // Attach this listener to every button,
         // which will set the view model for the direction activity
@@ -74,17 +95,18 @@ public class PlaceAdapter extends RecyclerView.Adapter<PlaceAdapter.PlaceViewHol
             public void onClick(View v) {
                 Context viewContext = v.getContext();
                 Intent intent = new Intent(viewContext, InstructionViewActivity.class);
-                intent.putExtra("NewPlace", currentPlace.getId());
+                intent.putExtra("NewPlace", currentPlaceViewModel.getId());
                 viewContext.startActivity(intent);
             }
         });
+
         holder.binding.executePendingBindings();
     }
 
 
     @Override
     public int getItemCount() {
-       return mPlaces.size();
+        return mPlaceEntities.size();
     }
 
 }
