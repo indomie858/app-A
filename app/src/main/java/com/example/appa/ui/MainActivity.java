33660caf2504684d976184186ff18afc13520846
Ryunosuke <ryunosuke.mori.294>
2020-11-04 13:14:27 +0900

    change fragment transaction methods

--- app/src/main/java/com/example/appa/ui/MainActivity.java
+++ app/src/main/java/com/example/appa/ui/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.preference.PreferenceManager;
 
 import com.example.appa.R;
 import com.example.appa.ui.home.HomeFragment;
@@ -35,30 +36,34 @@ public class MainActivity extends AppCompatActivity implements BluetoothDialog.B
 
         actionbar = (MaterialToolbar) findViewById(R.id.topAppBar);
 
-        fm.beginTransaction().add(R.id.main_container, tutorialFragment, "4").hide(tutorialFragment).commit();
-        fm.beginTransaction().add(R.id.main_container, settingsFragment, "2").hide(settingsFragment).commit();
-        fm.beginTransaction().add(R.id.main_container, homeFragment, "1").commit();
+        //fm.beginTransaction().replace(R.id.main_container, tutorialFragment, "4").hide(tutorialFragment).commit();
+        //fm.beginTransaction().replace(R.id.main_container, settingsFragment, "2").commit();
+        fm.beginTransaction().replace(R.id.main_container, homeFragment, "1").commit();
 
         // The switch case below is for adding the actions for when you click on the bottom menu -- create a case for the other buttons
         BottomNavigationView bottomNavigationView = (BottomNavigationView) findViewById(R.id.bottom_navigation);
         bottomNavigationView.setOnNavigationItemSelectedListener((item) ->{
             switch (item.getItemId()){
                 case R.id.home_button:
-                    fm.beginTransaction().hide(active).show(homeFragment).commit();
+                    fm.beginTransaction().replace(R.id.main_container, homeFragment, "1").commit();
+                    fm.beginTransaction().addToBackStack(null);
                     actionbar.setTitle("Home");
                     active = homeFragment;
                     backButtonFlag = false;
                     counter = 0;
                     break;
                 case R.id.settings_button:
-                    fm.beginTransaction().hide(active).show(settingsFragment).commit();
+                    //fm.beginTransaction().hide(active).show(settingsFragment).commit();
+                    fm.beginTransaction().replace(R.id.main_container, settingsFragment, "2").commit();
+                    fm.beginTransaction().addToBackStack(null);
                     actionbar.setTitle("Settings");
                     active = settingsFragment;
                     backButtonFlag = true;
                     counter = 0;
                     break;
                 case R.id.tutorial_button:
-                    fm.beginTransaction().hide(active).show(tutorialFragment).commit();
+                    fm.beginTransaction().replace(R.id.main_container, tutorialFragment, "4").commit();
+                    fm.beginTransaction().addToBackStack(null);
                     actionbar.setTitle("Tutorial");
                     active = tutorialFragment;
                     backButtonFlag = true;
