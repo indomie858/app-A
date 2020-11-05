d2997b44cde65852935cbab8c044fb8d7a71fbc2
Daniel <dpalencia2112@gmail.com>
2020-11-05 11:13:05 -0800

    Location permissions check - comment out

--- app/src/main/java/com/example/appa/ui/MainActivity.java
+++ app/src/main/java/com/example/appa/ui/MainActivity.java
@@ -89,7 +89,7 @@ public class MainActivity extends AppCompatActivity implements BluetoothDialog.B
             return false;
         });
 
-        checkLocationPermissions();
+        //checkLocationPermissions();
     }
 
     @Override
