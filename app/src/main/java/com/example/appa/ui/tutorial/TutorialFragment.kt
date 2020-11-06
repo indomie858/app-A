472993403f46a478b84116b767ac3706d45f0efa
Ryunosuke <ryunosuke.mori.294>
2020-11-06 14:12:03 +0900

    Edit Home card color to make easier to see letters

--- app/src/main/java/com/example/appa/ui/tutorial/TutorialFragment.kt
+++ app/src/main/java/com/example/appa/ui/tutorial/TutorialFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
 import com.example.appa.R
 import com.google.android.material.tabs.TabLayout
