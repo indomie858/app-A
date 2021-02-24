3f9638bbc8f8266b1e308e13d1e65f5f521cc6bc
Gaven <gavengrantz@gmail.com>
2021-02-24 15:51:43 -0800

    maneuver instruction list now updates along path

--- app/src/main/java/com/example/appa/ui/navigation/DirectionsActivity.kt
+++ app/src/main/java/com/example/appa/ui/navigation/DirectionsActivity.kt
@@ -35,10 +35,7 @@ import com.google.android.gms.tasks.OnSuccessListener
 import com.google.android.material.bottomsheet.BottomSheetBehavior
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.mapbox.android.core.location.*
-import com.mapbox.api.directions.v5.models.BannerInstructions
-import com.mapbox.api.directions.v5.models.DirectionsRoute
-import com.mapbox.api.directions.v5.models.RouteOptions
-import com.mapbox.api.directions.v5.models.VoiceInstructions
+import com.mapbox.api.directions.v5.models.*
 import com.mapbox.geojson.Point
 import com.mapbox.mapboxsdk.Mapbox
 import com.mapbox.mapboxsdk.camera.CameraUpdateFactory
@@ -668,6 +665,7 @@ class DirectionsActivity :
             val currentName = currentStep?.name()   //name of walkway
             val currentManeuver = currentStep?.maneuver()   //arraylist or json?
             val currentInstruction = currentManeuver?.instruction()
+            val currentStepIndex = currentStepProgress?.stepIndex
 
             val upcomingStep = currentLegProgress?.upcomingStep     //arraylist or json??
             val upcomingManeuver = upcomingStep?.maneuver()     //arraylist or json?
@@ -679,14 +677,19 @@ class DirectionsActivity :
             //NOTE: make sure to update directionsadapter if there is any changes to the structure of outputText
             val outputText = "$destinationName,$distanceRemaining,$distanceToNextStep,$upcomingInstruction"
 
-            var steps = routeProgress.route.legs()?.get(0)?.steps()
+            var steps: MutableList<LegStep>? = routeProgress.route.legs()?.get(0)?.steps()
             navigationText.text = outputText
             navigationData.clear()
             navigationData.add(outputText)
             if (steps != null) {
-                for (step in steps) {
-                    navigationData.add(step.maneuver().instruction().toString())
+                val endStepIndex = steps.size - 1;
+                for (i in currentStepIndex!!..endStepIndex) {
+                    navigationData.add(steps[i].maneuver().instruction().toString())
                 }
+
+//                for (step in steps) {
+//                    navigationData.add(step.maneuver().instruction().toString())
+//                }
             }
             adapter?.setData(navigationData)
             adapter?.notifyDataSetChanged()
@@ -717,7 +720,6 @@ class DirectionsActivity :
 
     private val bannerInstructionObserver = object : BannerInstructionsObserver {
         override fun onNewBannerInstructions(bannerInstructions: BannerInstructions){
-
         }
     }
 
