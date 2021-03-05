fb883db5f885caa9f0b632e9cf8eb6a274476aab
Gaven <gavengrantz@gmail.com>
2021-03-04 22:17:21 -0800

    created activity for directory info

---
+++ app/src/main/java/com/example/appa/ui/navigationlist/DirectoryInformationActivity.java
@@ -0,0 +1,23 @@
+package com.example.appa.ui.navigationlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.ActionBar;
+import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.example.appa.R;
+
+public class DirectoryInformationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_directory_information);
+
+
+
+    }
+
+
+}
